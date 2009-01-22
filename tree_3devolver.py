abfb930521db21d4e3c4b1b921b3fdbd36b197b2
Juan <juan@dragonite.(none)>
2009-01-21 22:58:49 -0800

    Adding new app for tree evolution.

--- tree_3devolver.py
+++ tree_3devolver.py
@@ -12,6 +12,103 @@ import evolve
 SPACE = 1000
 MOVE = 2000
 
+
+NEXTID = 1
+
+def tempName():
+    global NEXTID
+    id = NEXTID
+    NEXTID += 1
+    return 't%d'%id
+
+class OgreText(object):
+    """Class for displaying text in Ogre above a Movable."""
+    def __init__(self, movable, camera, text=''):
+        self.movable = movable
+        self.camera = camera
+        self.text = ''
+        self.enabled = True
+
+        ovm = ogre.OverlayManager.getSingleton()
+        self.overlay = ov = ovm.create(tempName())
+        self.container = c = ovm.createOverlayElement('Panel', tempName())
+        ov.add2D(c)
+        self.textArea = t = ovm.createOverlayElement('TextArea', tempName())
+        t.setDimensions(1.0, 1.0)
+        t.setMetricsMode(ogre.GMM_PIXELS)
+        t.setPosition(0, 0)
+        t.setParameter('font_name', 'BlueHighway')
+        t.setParameter('char_height', '16')
+        t.setParameter('horz_align', 'center')
+        t.setColour(ogre.ColourValue(1.0, 0.0, 0.0))
+        c.addChild(t)
+        ov.show()
+
+        self.setText(text)
+
+    def __del__(self):
+        self.destroy()
+
+    def destroy(self):
+        if hasattr(self, 'dead'): return
+        self.dead = True
+        self.overlay.hide()
+        ovm = ogre.OverlayManager.getSingleton()
+        self.container.removeChild(self.textArea.name)
+        self.overlay.remove2D(self.container)
+        ovm.destroyOverlayElement(self.textArea.name)
+        ovm.destroyOverlayElement(self.container.name)
+        ovm.destroy(self.overlay.name)
+
+    def enable(self, f):
+        self.enabled = f
+        if f:
+            self.overlay.show()
+        else:
+            self.overlay.hide()
+
+    def setText(self, text):
+        self.text = text
+        self.textArea.setCaption(ogre.UTFString(text))
+
+    def update(self):
+        if not self.enabled : return
+
+        # get the projection of the object's AABB into screen space
+        bbox = self.movable.getWorldBoundingBox(True);
+        mat = self.camera.getViewMatrix();
+        corners = bbox.getAllCorners();
+
+        min_x, max_x, min_y, max_y = 1.0, 0.0, 1.0, 0.0
+        # expand the screen-space bounding-box so that it completely encloses 
+        # the object's AABB
+        for corner in corners:
+            # multiply the AABB corner vertex by the view matrix to 
+            # get a camera-space vertex
+            corner = mat * corner;
+            # make 2D relative/normalized coords from the view-space vertex
+            # by dividing out the Z (depth) factor -- this is an approximation
+            x = corner.x / corner.z + 0.5
+            y = corner.y / corner.z + 0.5
+
+            if x < min_x: min_x = x
+            if x > max_x: max_x = x
+            if y < min_y: min_y = y
+            if y > max_y: max_y = y
+            
+        # we now have relative screen-space coords for the
+        # object's bounding box; here we need to center the
+        # text above the BB on the top edge. The line that defines
+        # this top edge is (min_x, min_y) to (max_x, min_y)
+
+        # self.container.setPosition(min_x, min_y);
+        # Edited by alberts: This code works for me
+        self.container.setPosition(1-max_x, min_y);
+        # 0.1, just "because"
+        self.container.setDimensions(max_x - min_x, 0.1);
+
+
+
 def get_width_height(cur_node):
     aab = cur_node.getAttachedObject(0).getBoundingBox();
     min = aab.getMinimum() * cur_node.getScale();
@@ -57,6 +154,7 @@ class GAListener(sf.FrameListener, OIS.MouseListener, OIS.KeyListener):
         OIS.MouseListener.__init__(self)
         OIS.KeyListener.__init__(self)
 
+        self.text_overlays = []
         self.toggle = 0
         self.mouse_down = False
 
@@ -126,6 +224,8 @@ class GAListener(sf.FrameListener, OIS.MouseListener, OIS.KeyListener):
     def frameStarted(self, frameEvent):
         if self.renderWindow.isClosed():
             return False
+
+        for ind in self.text_overlays: ind.update()
 
         self.Keyboard.capture()
         #self.Mouse.capture()
@@ -215,6 +315,7 @@ class GAListener(sf.FrameListener, OIS.MouseListener, OIS.KeyListener):
         '''
         Create and display a new pop.
         '''
+        text_overlays = []
         sceneManager = self.sceneManager
         sceneManager.destroyAllEntities()
         root_node = sceneManager.getRootSceneNode()
@@ -231,7 +332,13 @@ class GAListener(sf.FrameListener, OIS.MouseListener, OIS.KeyListener):
             sep_node = sceneManager.getSceneNode('Separator%d' % i)
             sep_node.removeAllChildren()
             ent = sceneManager.createEntity('Separator%d' %i, 'knot.mesh')
+
             sep_node.attachObject(ent)
+            t = OgreText(ent, self.camera, '%d' % i)
+            t.enable(True)
+            text_overlays.append(t)
+
+        self.text_overlays = text_overlays
 
 #---------------------------------#
     def makeCharacter(self, node_id, parent_node, genome = []):
